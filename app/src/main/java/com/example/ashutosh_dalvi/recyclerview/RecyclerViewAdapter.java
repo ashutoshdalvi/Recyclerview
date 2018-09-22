ee79c820bc533e4633368ec55d56e5f3fd9fbd2e
ashutoshdalvi <ashutoshdalvi29@gmail.com>
2018-09-22 17:36:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ashutosh_dalvi/recyclerview/RecyclerViewAdapter.java
@@ -0,0 +1,44 @@
+package com.example.ashutosh_dalvi.recyclerview;
+
+import android.nfc.Tag;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter  extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+    private static final String TAG = "RecyclerViewAdapter";
+
+   
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        CircleImageView image ;
+        TextView name;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image_view);
+            name = itemView.findViewById(R.id.name);
+        }
+    }
+
+
+}
